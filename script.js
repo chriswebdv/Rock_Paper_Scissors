c0af618d668b90e500201ff81685aaa524277491
chriswebdv <chriswebdv@gmail.com>
2023-05-01 16:45:27 -0600

    Updated script.js

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 
-const myArray = ['Rock', 'Paper', 'Scissors']; 
-function getComputerChoice(myArray){
+// Computer choice function
+
+const myArray = ['rock', 'paper', 'scissors']; 
+function getComputerChoice(){
     return myArray[(Math.random() * myArray.length) | 0];
 }
-console.log(getComputerChoice(myArray));
+
+//let ans = getComputerChoice();
+//console.log(ans);
+
+// single game function
+
+function playRound(playerSelection, computerSelection) {
+  str = `Your win! ${playerSelection.toLowerCase()} now ${computerSelection}`
+  return str;
+}
+   
+//let playerSelection = 'rock'
+const computerSelection = getComputerChoice();
+
+//playRound(playerSelection, computerSelection);
+
+// Five game function
+
+function game() {
+  fiveg = playRound(playerSelection,  computerSelection);
+  return fiveg;
+}
+let playerSelection = prompt('pick one:');
+const final = game();
+console.log(final);
+console.log(final);
+console.log(final);
+console.log(final);
+console.log(final);
+
+
